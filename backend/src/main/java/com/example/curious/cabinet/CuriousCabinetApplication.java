29ce3a30b22d7d12fd5da8c36df9ee9eaa6005cf
留行亭长丶 <zhuyifa1028@gmail.com>
2022-08-30 11:35:52 +0800

    好奇柜后端

---
+++ backend/src/main/java/com/example/curious/cabinet/CuriousCabinetApplication.java
@@ -0,0 +1,13 @@
+package com.example.curious.cabinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuriousCabinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuriousCabinetApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/curious/cabinet/CuriousCabinetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.curious.cabinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuriousCabinetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
