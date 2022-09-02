e5c3006a6015f212588d37cadca2a55d5a5e021a
留行亭长丶 <zhuyifa1028@gmail.com>
2022-09-02 16:03:04 +0800

    好奇柜数据库操作

---
+++ curious-cabinet-db/src/main/java/com/example/curious/cabinet/db/CuriousCabinetDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.curious.cabinet.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuriousCabinetDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuriousCabinetDbApplication.class, args);
+	}
+
+}
---
+++ curious-cabinet-db/src/test/java/com/example/curious/cabinet/db/CuriousCabinetDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.curious.cabinet.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuriousCabinetDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
