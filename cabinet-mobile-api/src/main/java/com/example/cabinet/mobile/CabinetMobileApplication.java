60d444f5db81e225f26e82c64039c741fdae6f36
留行亭长丶 <zhuyifa1028@gmail.com>
2022-11-15 22:16:42 +0800

    好奇柜移动端API

---
+++ cabinet-mobile-api/src/main/java/com/example/cabinet/mobile/CabinetMobileApplication.java
@@ -0,0 +1,13 @@
+package com.example.cabinet.mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetMobileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabinetMobileApplication.class, args);
+    }
+
+}
---
+++ cabinet-mobile-api/src/test/java/com/example/cabinet/mobile/CabinetMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cabinet.mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinetMobileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
