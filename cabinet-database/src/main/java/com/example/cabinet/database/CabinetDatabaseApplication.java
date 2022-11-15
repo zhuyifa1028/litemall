e64612613fbb445ce89fcf5150c43b10b2dba9b9
留行亭长丶 <zhuyifa1028@gmail.com>
2022-11-15 22:25:11 +0800

    好奇柜项目结构优化

---
+++ cabinet-database/src/main/java/com/example/cabinet/database/CabinetDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.cabinet.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinetDatabaseApplication.class, args);
+	}
+
+}
---
+++ cabinet-database/src/test/java/com/example/cabinet/database/CabinetDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cabinet.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinetDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
