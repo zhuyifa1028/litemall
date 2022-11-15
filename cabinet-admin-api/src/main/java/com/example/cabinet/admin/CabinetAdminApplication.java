d5e653a5511ec949a3eb9511c3b5d94960636b20
留行亭长丶 <zhuyifa1028@gmail.com>
2022-11-15 22:09:58 +0800

    好奇柜管理后台接口

---
+++ cabinet-admin-api/src/main/java/com/example/cabinet/admin/CabinetAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.cabinet.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabinetAdminApplication.class, args);
+    }
+
+}
