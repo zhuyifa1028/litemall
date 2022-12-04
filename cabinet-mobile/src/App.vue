0b6491874cd929541b0728a9eeb9bc920948d1f4
留行亭长丶 <zhuyifa1028@gmail.com>
2022-12-04 20:38:38 +0800

    好奇柜移动端首页50%

--- cabinet-mobile/src/App.vue
+++ cabinet-mobile/src/App.vue
@@ -1,11 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="cab-app">
+    <Home v-show="tabbarVisible === 0"></Home>
+
+    <nut-tabbar v-model:visible="tabbarVisible" active-color="#FFD339" unactive-color="#595959"
+                :bottom="true" :safeAreaInsetBottom="true">
+      <nut-tabbar-item tab-title="首页" icon="home">
+        <router-link to="/"></router-link>
+      </nut-tabbar-item>
+      <nut-tabbar-item tab-title="商城" icon="category"></nut-tabbar-item>
+      <nut-tabbar-item tab-title="发布" icon="find"></nut-tabbar-item>
+      <nut-tabbar-item tab-title="消息" icon="cart"></nut-tabbar-item>
+      <nut-tabbar-item tab-title="我的" icon="my"></nut-tabbar-item>
+    </nut-tabbar>
+  </div>
 </template>
+
+<script>
+import Home from '@/views/Home';
+
+export default {
+  components: {
+    Home
+  },
+  data() {
+    return {
+      tabbarVisible: 0
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -28,4 +51,5 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
 </style>
---
+++ cabinet-mobile/src/views/Home/Community.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="cab-home-community">
+    <nut-searchbar v-model="searchValue" placeholder="搜索内容或关键字">
+      <template v-slot:leftin>
+        <nut-icon size="14" name="search2"></nut-icon>
+      </template>
+      <template v-slot:rightin>
+        <nut-icon size="20" name="photograph"></nut-icon>
+      </template>
+    </nut-searchbar>
+    <nut-grid :border="false">
+      <nut-grid-item text="社群" :icon="require('@/assets/Home/Community/community.png')"></nut-grid-item>
+      <nut-grid-item text="直播" :icon="require('@/assets/Home/Community/broadcast.png')"></nut-grid-item>
+      <nut-grid-item text="拍卖" :icon="require('@/assets/Home/Community/auction.png')"></nut-grid-item>
+      <nut-grid-item text="藏品柜" :icon="require('@/assets/Home/Community/collection.png')"></nut-grid-item>
+    </nut-grid>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Community',
+  data() {
+    return {
+      searchValue: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
