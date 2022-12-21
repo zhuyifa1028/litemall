adbf5b9e01ba7cc733a46aa7d172d96be952b710
留行亭长丶 <zhuyifa1028@gmail.com>
2022-12-21 21:20:01 +0800

    正常打包

---
+++ litemall-api-common/src/main/java/com/github/litemall/common/constants/PackageConstants.java
@@ -0,0 +1,11 @@
+package com.github.litemall.common.constants;
+
+public interface PackageConstants {
+
+    String COMMON = "com.github.litemall.common";
+
+    String MANAGE = "com.github.litemall.manage";
+
+    String MOBILE = "com.github.litemall.mobile";
+
+}
--- litemall-api-manage/src/main/java/com/github/litemall/manage/ManageApiApplication.java
+++ litemall-api-manage/src/main/java/com/github/litemall/manage/ManageApiApplication.java
@@ -1,12 +1,16 @@
 package com.github.litemall.manage;
 
+import com.github.litemall.common.constants.PackageConstants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {
-        "com.github.litemall.manage",
-        "com.github.litemall.common"
-})
+@SpringBootApplication
+@ComponentScan({PackageConstants.COMMON, PackageConstants.MANAGE})
+@EntityScan({PackageConstants.COMMON})
+@EnableJpaRepositories({PackageConstants.COMMON})
 public class ManageApiApplication {
 
     public static void main(String[] args) {
