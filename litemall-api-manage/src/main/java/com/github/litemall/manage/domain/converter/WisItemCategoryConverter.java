9107bc2a9263fa588873bb59cb218dd1c3633597
孟冬廿八 <zhuyifa1028@gmail.com>
2023-02-20 10:45:59 +0800

    Gradle项目依赖版本统一管理

--- litemall-api-manage/src/main/java/com/github/litemall/manage/domain/converter/WisItemCategoryConverter.java
+++ litemall-api-manage/src/main/java/com/github/litemall/manage/domain/converter/WisItemCategoryConverter.java
@@ -3,11 +3,12 @@ package com.github.litemall.manage.domain.converter;
 import com.github.litemall.common.entity.item.WisItemCategory;
 import com.github.litemall.manage.domain.vo.WisItemCategorySaveVo;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 /**
  * 商品分类表Converter
  */
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface WisItemCategoryConverter {
 
     WisItemCategory toWisItemCategory(WisItemCategorySaveVo vo);
