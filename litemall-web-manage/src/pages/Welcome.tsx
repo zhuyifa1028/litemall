d01d7430b7cf04bc48c68d0d4919954a252e772e
孟冬廿八 <zhuyifa1028@gmail.com>
2023-06-24 17:19:10 +0800

    删除全球化

--- litemall-web-manage/src/pages/Welcome.tsx
+++ litemall-web-manage/src/pages/Welcome.tsx
@@ -1,7 +1,6 @@
 import {PageContainer} from '@ant-design/pro-components';
 import {Alert, Card, Typography} from 'antd';
 import React from 'react';
-import {FormattedMessage, useIntl} from 'umi';
 import styles from './Welcome.less';
 
 const CodePreview: React.FC = ({children}) => (
@@ -11,18 +10,12 @@ const CodePreview: React.FC = ({children}) => (
     </code>
   </pre>
 );
-
 const Welcome: React.FC = () => {
-  const intl = useIntl();
-
   return (
     <PageContainer>
       <Card>
         <Alert
-          message={intl.formatMessage({
-            id: 'pages.welcome.alertMessage',
-            defaultMessage: 'Faster and stronger heavy-duty components have been released.',
-          })}
+          message={'更快更强的重型组件，已经发布。'}
           type="success"
           showIcon
           banner
@@ -37,7 +30,7 @@ const Welcome: React.FC = () => {
             rel="noopener noreferrer"
             target="__blank"
           >
-            <FormattedMessage id="pages.welcome.link" defaultMessage="Welcome"/>
+            欢迎使用
           </a>
         </Typography.Text>
         <CodePreview>yarn add @ant-design/pro-components</CodePreview>
@@ -45,5 +38,4 @@ const Welcome: React.FC = () => {
     </PageContainer>
   );
 };
-
 export default Welcome;
